ec83f2ee3c60dbff0a1308ce1aa7991c2c7a6a0f
AlanSamBB <alansamps@gmail.com>
2020-01-27 21:07:19 -0800

    Initial Commit

---
+++ GMP.Factorial.Select/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GMP.Factorial.Select
+//
+//  Created by Alan Sampson on 1/27/20.
+//  Copyright © 2020 Alan @ FreeShell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
